f674056672fb3d3e73f0c21131de2c6f08a89348
Mahesh Prasanna <damp.dassanayake@gmail.com>
2022-12-20 16:48:27 +0530

    Finalize the UserDAOImpl

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `User`(
+    username VARCHAR(50) PRIMARY KEY ,
+    full_name VARCHAR(256) NOT NULL ,
+    password VARCHAR(300) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS `Project`(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    name VARCHAR(100) NOT NULL ,
+    username VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (username) REFERENCES `User` (username)
+    );
+
+CREATE TABLE IF NOT EXISTS `Task`(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    contact VARCHAR(500) NOT NULL ,
+    status ENUM('COMPLETED', 'NOTCOMPLETED') NOT NULL DEFAULT 'NOTCOMPLETED',
+    project_id INT NOT NULL,
+    CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES `Project` (id)
+    );
+
