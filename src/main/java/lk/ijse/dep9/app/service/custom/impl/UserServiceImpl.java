4679a70e3ade0ee973c7b2e3a72ab9a20b72b342
Mahesh Prasanna <damp.dassanayake@gmail.com>
2022-12-21 14:26:07 +0530

    Revamp process-3

--- src/main/java/lk/ijse/dep9/app/service/custom/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package lk.ijse.dep9.app.service.custom.impl;
 
 
+import lk.ijse.dep9.app.dao.custom.UserDAO;
 import lk.ijse.dep9.app.dto.UserDTO;
 import lk.ijse.dep9.app.entity.User;
 import lk.ijse.dep9.app.service.custom.UserService;
+import lk.ijse.dep9.app.util.Transformer;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +18,17 @@ import java.sql.SQLException;
 @Component
 public class UserServiceImpl implements UserService {
 
-    public UserServiceImpl() {
+    private final UserDAO userDAO;
 
+    private final Transformer transformer;
+
+    public UserServiceImpl(UserDAO userDAO, Transformer transformer) {
+        this.userDAO = userDAO;
+        this.transformer = transformer;
     }
 
     @Override
     public void createNewUserAccount(UserDTO userDTO) {
-
+        userDAO.save(transformer.toUser(userDTO));
     }
 }
