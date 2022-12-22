f9c9bb1a975d6c257f69e44f2487b6335f4ce5ae
Mahesh Prasanna <damp.dassanayake@gmail.com>
2022-12-22 14:18:57 +0530

    Implement entity dto conversion for Project

--- src/main/java/lk/ijse/dep9/app/service/custom/impl/ProjectTaskServiceImpl.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/impl/ProjectTaskServiceImpl.java
@@ -1,8 +1,40 @@
 package lk.ijse.dep9.app.service.custom.impl;
 
+import lk.ijse.dep9.app.dao.custom.ProjectDAO;
+import lk.ijse.dep9.app.dao.custom.TaskDAO;
+import lk.ijse.dep9.app.dto.ProjectDTO;
 import lk.ijse.dep9.app.service.custom.ProjectTaskService;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ProjectTaskServiceImpl implements ProjectTaskService {
+
+    private ProjectDAO projectDAO;
+    private TaskDAO taskDAO;
+    @Override
+    public ProjectDTO createNewProject(ProjectDTO projectDTO) {
+        return null;
+    }
+
+    @Override
+    public List<ProjectDTO> getAllProjects(String username) {
+        return null;
+    }
+
+    @Override
+    public ProjectDTO getProjectDetails(String username, int projectId) {
+        return null;
+    }
+
+    @Override
+    public void renameProject(ProjectDTO project) {
+
+    }
+
+    @Override
+    public void deleteProject(String username, int projectId) {
+
+    }
 }
--- src/main/java/lk/ijse/dep9/app/util/Transformer.java
+++ src/main/java/lk/ijse/dep9/app/util/Transformer.java
@@ -1,6 +1,8 @@
 package lk.ijse.dep9.app.util;
 
+import lk.ijse.dep9.app.dto.ProjectDTO;
 import lk.ijse.dep9.app.dto.UserDTO;
+import lk.ijse.dep9.app.entity.Project;
 import lk.ijse.dep9.app.entity.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -20,5 +22,13 @@ public class Transformer {
 
     public UserDTO toUserDTO(User user){
         return mapper.map(user, UserDTO.class);
+    }
+
+    public Project toProject(ProjectDTO dto){
+        return mapper.map(dto, Project.class);
+    }
+
+    public ProjectDTO toProjectDTO(Project projectEntity){
+        return mapper.map(projectEntity, ProjectDTO.class);
     }
 }
